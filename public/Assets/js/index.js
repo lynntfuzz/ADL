2513b49c5e353f02ddc1043bdc25bce84adef98f
acuteangel <akaquas@gmail.com>
2018-12-20 15:49:48 -0800

    saving work

---
+++ public/Assets/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 300) {
+        $("#navbar-row-0").show("slow");
+        $("#navbar-row-1").hide();
+        $("#navbar-row-2").hide();
+        $("#navbar-row-3").hide();
+      } else if ($(document).scrollTop() < 25){
+        $("#navbar-row-0").hide();
+        $("#navbar-row-1").show();
+        $("#navbar-row-2").show();
+        $("#navbar-row-3").show();
+      }
+    });
+  });
