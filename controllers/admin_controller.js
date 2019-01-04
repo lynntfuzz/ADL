51808718874346c6e7ce33c05269a7ab118e69ef
Lynn Thompson <lynninla@earthlink.net>
2019-01-03 20:17:42 -0800

    Changed routes of admin portal and moved login to partials

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -1,10 +1,24 @@
 var db  = require('../models');
 
+// exports.index = function(req, res) {
+//   console.log("admin_controller.index");
+//   res.render('login/login',{
+//     layout: 'admin'
+//  })
+// };
+
 exports.index = function(req, res) {
-  console.log("admin_controller.index");
-  res.render('login/login',{
-    layout: 'admin'
- })
+  db.Contact.findAll({ order: [['createdAt', 'DESC']]
+    }).then(function(dbContacts) {
+      db.Search.findAll({}).then(function(dbSearch){
+        res.render('admin-portal', {
+          layout: 'admin',
+          contact: dbContacts,
+          search: dbSearch
+        });
+      });
+    });
+  
 };
 
 exports.all_contacts = function(req, res) {
